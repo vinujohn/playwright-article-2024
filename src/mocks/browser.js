892e5e003dca9721114632481c57e2f65899cba5
Artem Sapegin <artem@sapegin.ru>
2024-05-07 13:39:26 +0200

    Expose all MSW methods on window

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,9 +1,9 @@
+import * as msw from 'msw';
 import { setupWorker } from 'msw/browser';
-import { http, HttpResponse } from 'msw';
 import { handlers } from './handlers';
 
-// This configures a Service Worker with the given request handlers
+// Configure a Service Worker with the given request handlers
 export const worker = setupWorker(...handlers);
 
-// Expose method globally to make them available in integration tests
-window.msw = { worker, http, HttpResponse };
+// Expose methods globally to make them available in integration tests
+window.msw = { worker, ...msw };
