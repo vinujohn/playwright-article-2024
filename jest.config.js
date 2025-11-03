5642b3fa2f54660f4db1f3bc7cbdec9b1f5786af
Vinu John <vinu.john.work@gmail.com>
2025-11-03 11:01:40 -0500

    response b

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  //setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.js',
+    '!src/reportWebVitals.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
